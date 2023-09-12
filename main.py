b440eaed2127544f382220f3a855c38c41b7af57
MoriomPouspo <64324298+MoriomPouspo@users.noreply.github.com>
2023-09-13 01:30:09 +0600

    finalize

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import csv
+import pymongo
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+attributes = [
+    {'name': 'Gender', 'type': 'string', 'domain': '{"M","F"}'},
+    {'name': 'AgeBaseline', 'type': 'int', 'domain': '[0, 100]'},
+    {'name': 'HTNmeds', 'type': 'int', 'domain': '{0,1}'},
+    {'name': 'ACEIARB', 'type': 'int', 'domain': '{0,1}'},
+    {'name': 'CholesterolBaseline', 'type': 'float', 'domain': '{2.23, ...,9.3}'},
+    {'name': 'HistoryDiabetes', 'type': 'bool'},
+    {'name': 'HistoryCHD', 'type': 'bool'},
+    {'name': 'HistoryVascular', 'type': 'bool'},
+    {'name': 'HistorySmoking', 'type': 'bool'},
+    {'name': 'HistoryHTN', 'type': 'bool'},
+    {'name': 'HistoryDLD', 'type': 'bool'},
+]
+
+print("Attributes")
+for attr in attributes:
+    print("|", attr)
+
+data_list = []
+with open('dummy_data.csv', 'r') as csv_file:
+    # Create a CSV DictReader object
+    csv_reader = csv.DictReader(csv_file)
+
+    for row in csv_reader:
+        data = {'id': row['id'], 'date': row['date'], 'patientname': row['patientname'], 'drname': row['drname']}
+
+        for attr in attributes:
+            value = ''
+            if attr['name'] in row:
+                value = row[attr['name']]
+
+            data[attr['name']] = value
+
+        data_list.append(data)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Establish a connection to your MongoDB server
+client = pymongo.MongoClient("mongodb://admin:admin@localhost:27017/?authMechanism=DEFAULT")
 
+# Select a database and collection
+db = client["medchain"]
+collection = db["diagnostics"]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Insert the list of dictionaries into the collection
+result = collection.insert_many(data_list)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Print the inserted document IDs
+print("Inserted document IDs:", result.inserted_ids)
